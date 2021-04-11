636ec6e3e3f16c5abe64ffd7db52001d7b2ed85f
liaolonghui <1340881930@qq.com>
2021-04-11 13:27:36 +0800

    思考如何抽离重复的逻辑代码

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="container">
     <global-header :user="user" />
+    <!-- form -->
+    <form action="">
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" v-model="emailRef.val" @blur="validateEmail">
+        <small class="form-text" v-if="emailRef.error">{{ emailRef.message }}</small>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">密码</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+      </div>
+    </form>
+    <!-- columnList -->
     <column-list :list="list" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
@@ -55,6 +68,9 @@ const testData: ColumnProps[] = [
   }
 ]
 
+// 邮箱的正则
+const emailReg = /^[a-zA-Z0-9]+([-_.][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([-_.][a-zA-Z0-9]+)*\.[a-z]{2,}$/
+
 export default defineComponent({
   name: 'App',
   components: {
@@ -62,9 +78,28 @@ export default defineComponent({
     GlobalHeader
   },
   setup () {
+    const emailRef = reactive({
+      val: '',
+      error: false,
+      message: ''
+    })
+    const validateEmail = () => {
+      const emailVal = emailRef.val.trim()
+      if (!emailVal) {
+        emailRef.error = true
+        emailRef.message = '邮箱不能为空'
+      } else if (!emailReg.test(emailVal)) {
+        emailRef.error = true
+        emailRef.message = '请输入正确的邮箱地址'
+      }
+    }
+
     return {
       list: testData,
-      user: testUser
+      user: testUser,
+      // 邮箱验证等
+      emailRef,
+      validateEmail
     }
   }
 })
